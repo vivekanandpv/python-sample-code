9507f05e122c3e88cb118aeb15a292bf5a8db78c
Vivekanand P V <vivekanandpv@outlook.com>
2021-11-10 11:24:13 +0530

    solution added

---
+++ py-solution-01.py
@@ -0,0 +1,46 @@
+states_capitals = {
+    "Andhra Pradesh": "Amaravati",
+    "Arunachal Pradesh": "Itanagar",
+    "Assam": "Dispur",
+    "Bihar": "Patna",
+    "Chhattisgarh": "Raipur",
+    "Goa": "Panaji",
+    "Gujarat": "Gandhinagar",
+    "Haryana": "Chandigarh",
+    "Himachal Pradesh": "Shimla",
+    "Jharkhand": "Ranchi",
+    "Karnataka": "Bengaluru",
+    "Kerala": "Thiruvananthapuram",
+    "Madhya Pradesh": "Bhopal",
+    "Maharashtra": "Mumbai",
+    "Manipur": "Imphal",
+    "Meghalaya": "Shillong",
+    "Mizoram": "Aizawl",
+    "Nagaland": "Kohima",
+    "Odisha": "Bhubaneswar",
+    "Punjab": "Chandigarh",
+    "Rajasthan": "Jaipur",
+    "Sikkim": "Gangtok",
+    "Tamil Nadu": "Chennai",
+    "Telangana": "Hyderabad",
+    "Tripura": "Agartala",
+    "Uttar Pradesh": "Lucknow",
+    "Uttarakhand": "Dehradun",
+    "West Bengal": "Kolkata",
+}
+
+#   iterating over states
+for state in states_capitals:
+    print(f'State: {state}')
+
+print('~'*50)
+
+#   iterating over capitals
+for capital in states_capitals.values():
+    print(f'Capital: {capital}')
+
+print('~'*50)
+
+#   iterating over the dictionary and producing string representation
+for state, capital in states_capitals.items():
+    print(f'Capital of {state} is {capital}')
